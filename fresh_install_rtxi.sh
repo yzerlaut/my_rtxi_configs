1da2ea85c1579e3f4647d496caa0c095306c6e28
yzerlaut <yann.zerlaut@gmail.com>
2018-07-02 14:48:22 +0200

    up

--- fresh_install_rtxi.sh
+++ fresh_install_rtxi.sh
@@ -3,7 +3,8 @@ ssh-keygen -t rsa -b 4096 -C "yann.zerlaut@gmail.com"
 eval "$(ssh-agent -s)"
 ssh-add -K ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub
-# install emacs
+read -p "[...] =========== >> Copy the following key in the list of github ssh keys [Press anykey to continue]" none
+#install emacs
 sudo add-apt-repository ppa:kelleyk/emacs
 sudo apt update
 sudo apt install emacs25
